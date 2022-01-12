b4896f56392775fe1342cceff2c921fc11a16c57
Steven Lio <stevenlio88@yahoo.com.hk>
2022-01-11 22:29:55 -0800

    src: Created ImgCompress.py

---
+++ src/ImgHelp/ImgCompress.py
@@ -0,0 +1,27 @@
+def ImgCompress(img, method, level=1):
+    """
+    Image compression using different compression method.
+
+    This function compress image to a user defined compression level.
+
+    Parameters
+    ----------
+    img : numpy.array
+        Input image for compression.
+    method : string
+        Compression methods: Resize, SVD
+    level : int
+        Level of compression: 1-High, 2-Med, 3-Low
+
+    Returns
+    -------
+    numpy.array
+        The compressed image.
+
+
+    Examples
+    --------
+    >>> ImgCompress(img, 'Resize', level)
+    """
+    
+    pass
