5e0c4f260742875ef593930547222018d3ec11da
sufang <sufangtan.ca@gmail.com>
2022-01-20 15:48:17 -0800

    add function and function test

--- src/ImgHelp/ColorConv.py
+++ src/ImgHelp/ColorConv.py
@@ -1,17 +1,43 @@
+import numpy as np
 def ColorConv(img, color):
     """
-    ColorCov converts an image to different color.
+    ColorConv converts an image to different color.
     Parameters
     ----------
     img : numpy.array
-        Input a 2D np.array image for convertion.
+        Input a 3D np.array image for convertion.
     color : string
-        Color aim to convert: 'gray', 'black'
+        Color aim to convert: 'gray', 'rad','green','blue'
     Returns
     -------
     numpy.array
-        The converted image as 2D np.array.
+        The converted image as 3D np.array.
     Examples
     --------
     >>> ColorConv(img, 'gray')
     """
+    assert isinstance(color, str), "Invalid image type, expecting numpy.ndarray."
+    assert len(img.shape()) == 3, "Invalid image type, expecting numpy.3d array."
+    assert img.shape[2] == 3, "Invalid image type, expecting a RGB image."
+    assert isinstance(color, str), 'Color is not a string!'
+    assert isinstance(img, len(a.shape)), 'Invalid color type!'
+
+    if color = 'gray':
+        conv_img = np.mean(img, axis=2)
+        return conv_img
+    elif color = 'rad':
+        conv_img = img.copy()
+        conv_img[:,:1] = np.zeros(ubc.shape[2:])
+        conv_img[:,:2] = np.zeros(ubc.shape[2:])
+        return conv_img
+    elif color = 'green':
+        conv_img = img.copy()
+        conv_img[:,:0] = np.zeros(ubc.shape[2:])
+        conv_img[:,:2] = np.zeros(ubc.shape[2:])
+        return conv_img
+        return conv_img
+    elif color = 'blue':
+        conv_img = img.copy()
+        conv_img[:,:0] = np.zeros(ubc.shape[2:])
+        conv_img[:,:1] = np.zeros(ubc.shape[2:])
+        return conv_img
