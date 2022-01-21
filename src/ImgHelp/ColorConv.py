cf7cb64690ff01e9e0b8b1be60a7210db512bba3
sufang <sufangtan.ca@gmail.com>
2022-01-20 17:04:45 -0800

    Bug fixed

--- src/ImgHelp/ColorConv.py
+++ src/ImgHelp/ColorConv.py
@@ -27,17 +27,17 @@ def ColorConv(img, color):
         return conv_img
     elif color = 'rad':
         conv_img = img.copy()
-        conv_img[:,:1] = np.zeros(ubc.shape[2:])
-        conv_img[:,:2] = np.zeros(ubc.shape[2:])
+        conv_img[:,:1] = np.zeros(conv_img.shape[2:])
+        conv_img[:,:2] = np.zeros(conv_img.shape[2:])
         return conv_img
     elif color = 'green':
         conv_img = img.copy()
-        conv_img[:,:0] = np.zeros(ubc.shape[2:])
-        conv_img[:,:2] = np.zeros(ubc.shape[2:])
+        conv_img[:,:0] = np.zeros(conv_img.shape[2:])
+        conv_img[:,:2] = np.zeros(conv_img.shape[2:])
         return conv_img
         return conv_img
     elif color = 'blue':
         conv_img = img.copy()
-        conv_img[:,:0] = np.zeros(ubc.shape[2:])
-        conv_img[:,:1] = np.zeros(ubc.shape[2:])
+        conv_img[:,:0] = np.zeros(conv_img.shape[2:])
+        conv_img[:,:1] = np.zeros(conv_img.shape[2:])
         return conv_img
